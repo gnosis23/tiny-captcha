29fb41ed533a95f9c85726bde044f6d78d3013c7
Bohao Wang <bj050323@gmail.com>
2024-03-08 22:10:51 +0800

    feat: authorization

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tiny-captcha
+Captcha code service, a demo showcasing the integrated application of express, jwt, and docker.
---
+++ main.js
@@ -0,0 +1,48 @@
+const process = require('process')
+const jwt = require('jsonwebtoken')
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+const secretKey = process.env.SECRET_KEY
+
+const checkAuth = (authorization) => {
+	if (!authorization) throw new Error('Missing Authorization header!')
+	if (Array.isArray(authorization)) throw new Error('Which Authorization header?')
+	if (!authorization.startsWith('Bearer '))
+		throw new Error('Invalid Authorization header format!')
+
+	const token = authorization.split(' ')[1]
+	// will throw if invalid token
+	const payload = jwt.verify(token, secretKey)
+
+	// add more conditions here if you need
+	console.log('payload', payload)
+	return true
+}
+
+app.get('/api/captcha/get', (req, res) => {
+	try {
+		checkAuth(req.headers['authorization'])
+	} catch (err) {
+		res.status(401).send(err.message)
+		return
+	}
+
+	res.send('get /api/captcha/get success!')
+})
+
+app.post('/api/captcha/check', (req, res) => {
+	try {
+		checkAuth(req.headers['authorization'])
+	} catch (err) {
+		res.status(401).send(err.message)
+		return
+	}
+
+	res.send('/api/captcha/check')
+})
+
+app.listen(port, () => {
+	console.log(`Example app start: http://localhost:${port}`)
+})

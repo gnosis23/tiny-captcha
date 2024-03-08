feee26cb1d5a0c0c170538d712452fb7065acd61
Bohao Wang <bj050323@gmail.com>
2024-03-08 22:52:28 +0800

    feat: get captcha

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const process = require('process')
 const jwt = require('jsonwebtoken')
 const express = require('express')
+const svgCaptcha = require('svg-captcha')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
 const port = process.env.PORT || 3000
 const secretKey = process.env.SECRET_KEY
+
+const keyMap = {}
 
 const checkAuth = (authorization) => {
 	if (!authorization) throw new Error('Missing Authorization header!')
@@ -17,7 +21,7 @@ const checkAuth = (authorization) => {
 	const payload = jwt.verify(token, secretKey)
 
 	// add more conditions here if you need
-	console.log('payload', payload)
+	// console.log('payload', payload)
 	return true
 }
 
@@ -29,7 +33,15 @@ app.get('/api/captcha/get', (req, res) => {
 		return
 	}
 
-	res.send({ code: 0, message: 'success', data: { key: 123 } })
+	// create captcha
+	const result = svgCaptcha.create({ noise: 4, color: true })
+	const key = uuidv4()
+
+	// TODO: clear expired
+	keyMap[key] = result.text
+
+	// res.type('svg').send(result.data).end();
+	res.send({ code: 0, message: 'success', data: { image: result.data, key } })
 })
 
 app.post('/api/captcha/check', (req, res) => {
